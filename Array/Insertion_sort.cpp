8394094496968f951fd4ad55ca63d4d8029ac36c
DivyaSharma0894 <120556784+DivyaSharma0894@users.noreply.github.com>
2024-04-27 08:40:44 +0530

    sorting

--- Array/Insertion_sort.cpp
+++ Array/Insertion_sort.cpp
@@ -1,15 +1,31 @@
 #include<iostream>
 using namespace std;
 void Insertion_sort(int arr[],int n){
-    for(int i=1;i<n;i++){
-        for(int j=0;j<=i;j++){
-            if(arr[i]<arr[j]){
+    for(int i=1;i<n;i++){int temp=arr[i];
+    int j;
+        for( j=i-1;j>=0;j--){
+            if(arr[j]>temp) 
+            {arr[j+1]=arr[j];
+               
                 
             }
+
         }
+            {  arr[j+1]=temp;}
+
     }
 
 }
 int main(){
+    int n;
+    cin>>n;
+    int arr[10]={0};
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Insertion_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
 return 0;
 }
